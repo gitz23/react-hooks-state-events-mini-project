07c94638cf8f21f04aacf4fd8e2b9e86ee2dfe4d
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-07-24 12:32:49 +0300

    done with taskList test

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Task from "../components/Task"
 
-function TaskList() {
+function TaskList({tasks}) {
+
+  const newTaskList = tasks.map((task, index)=>{
+    return <Task key={index} text={task.text} category={task.category}/>
+  })
+
   return (
     <div className="tasks">
+      {newTaskList}
       {/* display a list of tasks using Task component */}
     </div>
   );
