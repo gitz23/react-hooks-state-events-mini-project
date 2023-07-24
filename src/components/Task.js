674eb3c330ff7868254373ef5ceebc9dddc2cdda
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-07-24 13:28:33 +0300

    done with task remaining with delete

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { useState } from "react";
 
-function Task({text, category }) {
+function Task({text, category, key}) {
 
-  // const [X , setX] = useState("X")
+  const [oldTasks, setOldTask] =  useState({text, category})
+  console.log({text, category})
 
-  // function deleteTask(){
-  //   onClick({ 
-  //     text : ""
-  //   })
-  // }onClick={deleteTask}
+  function deleteTask(key){
+    const newlistArray = oldTasks.filter((task)=> task.key !== key);
+      setOldTask(newlistArray);
+    }
+  
 
   return (
     <div className="task">
       <div className="label">{text}</div>
       <div className="text">{category}</div>
-      <button className="delete">X</button>
+      <button className="delete" onClick={() => deleteTask(key)}>X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,17 +1,13 @@
 import React from "react";
 import Task from "../components/Task"
+import { useState } from "react";
 
 function TaskList({tasks}) {
 
-  // function deleteTask(){
-
-  // }
-
   const newTaskList = tasks.map((task)=>{
-    return (<li key={task} >
-    <Task text={task.text} category={task.category} 
-    //onClick={deleteTask}
-    /></li>)
+    return (<li > 
+    <Task key={task} text={task.text} category={task.category} />
+    </li>)
   })
 
   return (
